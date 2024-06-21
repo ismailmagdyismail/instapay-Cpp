58a4c200df1291f10682b5f581d5caafc494d95a
Magdy, Ismail (ext) (DI SW ICS MNA RD HAV1 A) <ismail.magdy.ext@siemens.com>
2024-06-22 01:59:04 +0300

    change cmake

--- src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "InstapayServer.h"
-int main(){
-    InstapayServer* instapayServer = InstapayServer::getInstance();
-    instapayServer->run();
-    delete instapayServer;
-}
