13de21da0f0e83c297b5120a36a37354be580611
Magdy, Ismail (ext) (DI SW ICS MNA RD HAV1 A) <ismail.magdy.ext@siemens.com>
2024-07-01 21:21:50 +0300

    testing purposes

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,8 @@ void test(A a){
 
 int main(){
     B(A{});
-    std::cout<<"Test 2 for DEMO purposess";
+    std::cout<<"Test 2 for DEMO purposess"
+    std::cout<<"testing again\n";
     // std::cout<<"Hello world";
    InstapayServer* instapayServer = InstapayServer::getInstance();
    instapayServer->run();
