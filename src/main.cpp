2de13b5e5bc4dded6e83f9cf6aded7166212ed25
Magdy, Ismail (ext) (DI SW ICS MNA RD HAV1 A) <ismail.magdy.ext@siemens.com>
2024-07-01 21:36:13 +0300

    add Server entry point

--- src/main.cpp
+++ src/main.cpp
@@ -4,44 +4,8 @@
 #include <thread>
 #include "../cpp-httplib/httplib.h"
 
-class Test{
-  public:
-    virtual void test() = 0 ;
-    virtual ~Test() = default;
-};
-
-class A{
-public :
-    A() {std::cout<<"DEF\n";}
-    A(const A& other){
-        std::cout<<"COPY\n";
-    }
-    A(A&& other ){
-        std::cout<<"MOVE\n";
-    }
-};
-
-class B{
-public:
-    B(const A& a):a(a){
-    }
-    B(A&& a):a(std::move(a)){
-    }
-    A a;
-};
-void test(A a){
-    std::cout<<"test\n";
-}
-
 int main(){
-    B(A{});
-    std::cout<<"Test 2 for DEMO purposess";
-    std::cout<<"testing again\n";
-    std::cout<<"testing again\n";
-
-    // std::cout<<"Hello world";
-   InstapayServer* instapayServer = InstapayServer::getInstance();
-   instapayServer->run();
-   delete instapayServer;
-
+    InstapayServer* instapayServer = InstapayServer::getInstance();
+    instapayServer->run();
+    delete instapayServer;
 }
